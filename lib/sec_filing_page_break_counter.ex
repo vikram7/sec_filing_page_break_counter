38af1db4d72e6ee70a9cdccaee5ab79d32f0f717
Vikram Ramakrishnan <lord.ezar@gmail.com>
2016-02-27 23:44:53 -0500

    add placeholder counter function & test

--- lib/sec_filing_page_break_counter.ex
+++ lib/sec_filing_page_break_counter.ex
@@ -1,2 +1,8 @@
 defmodule SecFilingPageBreakCounter do
+
+  def page_break_count(string) do
+    string
+    |> String.split("page-break-after")
+    |> length
+  end
 end
--- test/sec_filing_page_break_counter_test.exs
+++ test/sec_filing_page_break_counter_test.exs
@@ -2,7 +2,10 @@ defmodule SecFilingPageBreakCounterTest do
   use ExUnit.Case
   doctest SecFilingPageBreakCounter
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "#page_break_count returns the count of page breaks in an HTML document" do
+    string = File.read!("test/fixtures/nflx.txt")
+
+    count = string |> SecFilingPageBreakCounter.page_break_count
+    assert count == 67
   end
 end
